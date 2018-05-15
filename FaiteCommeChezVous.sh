77bd58cb42edb6f998db8114ba36e8a08d9cf4d6
MustyMusty <r.quadrelli@hotmail.fr>
2018-05-15 23:00:45 +0200

    Le commit qui met mal

--- FaiteCommeChezVous.sh
+++ FaiteCommeChezVous.sh
@@ -3,16 +3,24 @@
 #Deploy my dayum .env
 
 #Add our /bin path for binaries 
-export PATH=/home/mustyfrog/Documents/Perso/env/bin:$PATH
+export PATH=$PWD/bin:$PATH
 
 #Indicate location of shared Library (for firefox Libhunspell-1.3.so.0)
-export LD_LIBRARY_PATH=/home/mustyfrog/Documents/Perso/env/share
+export LD_LIBRARY_PATH=$PWD/share
 
 #Indicate wich text editor to use (Vim for the boss)
 export EDITOR=vim
 
 
 #Create a child bash to implement all my exports because when we change environment variables
+#Copie du fichier des bookMark Firefox (manque plus qu'à faire la backup dans Firefox)
+#Pour backup (dans Firefox) => View all bookMark => Restore => May 15 
+touch MaisQueFaitIl
+ls ~/.mozilla/firefox/ |grep .default > MaisQueFaitIl
+boutDeChemin=$(cat MaisQueFaitIl)
+cp files/meinBookmarks.json ~/.mozilla/firefox/$boutDeChemin/bookmarkbackups/
+
+
 #in a bash script, it creates a child process and we it's done, changes are deleted with the process
 #So i'm creating a new bash process with theses changes
 exec /bin/bash
@@ -20,9 +28,4 @@ exec /bin/bash
 
 
 
-#Copie du fichier des bookMark Firefox (manque plus qu'à faire la backup dans Firefox)
-#Pour backup (dans Firefox) => View all bookMark => Restore => May 15 
-ls ~/.mozilla/firefox/ |grep .default > MaisQueFaitIl
-boutDeChemin=$(cat MaisQueFaitIl)
-cp files/meinBookmarks.json ~/.mozilla/firefox/$boutDeChemin/bookmarkbackups/
 
