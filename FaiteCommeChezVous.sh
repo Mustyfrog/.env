a64d99e9137628ee6ea7bb93101b4fc320dd6de4
mustyfrog <mustyfrog@mustyfrog.DOMIPERION>
2018-05-16 08:17:31 +0200

    Ich mochte ein beer bitechön...

--- FaiteCommeChezVous.sh
+++ FaiteCommeChezVous.sh
@@ -16,10 +16,10 @@ export EDITOR=vim
 #Copie du fichier des bookMark Firefox (manque plus qu'à faire la backup dans Firefox)
 #Pour backup (dans Firefox) => View all bookMark => Restore => May 15 
 touch MaisQueFaitIl
-ls ~/.mozilla/firefox/ |grep .default > MaisQueFaitIl
+ls ~/.mozilla/firefox|grep .default > MaisQueFaitIl
 boutDeChemin=$(cat MaisQueFaitIl)
 cp files/meinBookmarks.json ~/.mozilla/firefox/$boutDeChemin/bookmarkbackups/
-
+rm MaisQueFaitIl
 
 #in a bash script, it creates a child process and we it's done, changes are deleted with the process
 #So i'm creating a new bash process with theses changes
